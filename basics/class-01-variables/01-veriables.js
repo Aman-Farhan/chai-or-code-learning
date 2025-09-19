10dfc78aa1adbd2b7b1fdeeea72bdf684c4c6253
Aman <amanfarhan2008@gmail.com>
2025-09-19 16:45:19 +0500

    jsd

---
+++ basics/class-01-variables/01-veriables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "amanfarhan2008@gmail.com";
+var accountPassword = "123456";
+accountCity = "Karachi";
+let accountState;
+
+// accountId = 2 // is not allowed in javascript:
+
+console.log(accountId);
+
+accountEmail = "amanfarhan2025@gmail.com";
+accountPassword = "098765";
+accountCity = "Islamabad";
+accountState = "xyz";
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+
+
