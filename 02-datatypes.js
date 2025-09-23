eac9c8c5903be31f8abd511d61a77fbd00f2c5e6
Aman <amanfarhan2008@gmail.com>
2025-09-24 00:37:45 +0500

    second comment

---
+++ 02-datatypes.js
@@ -0,0 +1,8 @@
+// let a = alert(3+3);
+// console.log(a);
+
+let name = "Aman";  // string data type :
+let age = 17;  //  number date type :
+let isLoggedIn = true;   // boolean data type : 
+
+console.log(typeof age);
